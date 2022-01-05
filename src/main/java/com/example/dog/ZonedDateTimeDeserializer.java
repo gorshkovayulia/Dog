0592506b55e1122888f33028990cc37cb206b63f
iuliia_gorshkova <gorshkovayulia93@gmail.com>
2022-01-05 14:30:36 +0300

    Add JavaDoc to Serializer and Deserializer classes + fix pom.xml and web.xml files

--- src/main/java/com/example/dog/ZonedDateTimeDeserializer.java
+++ src/main/java/com/example/dog/ZonedDateTimeDeserializer.java
@@ -1,7 +1,6 @@
 package com.example.dog;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 
@@ -9,10 +8,14 @@ import java.io.IOException;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * The standard Jackson's implementation is not appropriate since it looses the original timezone information
+ * (DateTimeFormatter uses ISO_OFFSET_DATE_TIME constant, so dateOfBirth.zone.id is always "UTC")
+ */
 public class ZonedDateTimeDeserializer extends JsonDeserializer<ZonedDateTime> {
 
     @Override
-    public ZonedDateTime deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+    public ZonedDateTime deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
         return ZonedDateTime.parse(jp.getText(), DateTimeFormatter.ISO_DATE_TIME);
     }
 }
--- src/main/java/com/example/dog/ZonedDateTimeSerializer.java
+++ src/main/java/com/example/dog/ZonedDateTimeSerializer.java
@@ -1,7 +1,6 @@
 package com.example.dog;
 
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
@@ -9,6 +8,10 @@ import java.io.IOException;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * The standard Jackson's implementation is not appropriate since it looses the original timezone information
+ * (DateTimeFormatter uses ISO_OFFSET_DATE_TIME constant, so dateOfBirth.zone.id is always "UTC")
+ */
 public class ZonedDateTimeSerializer extends JsonSerializer<ZonedDateTime> {
 
     @Override
