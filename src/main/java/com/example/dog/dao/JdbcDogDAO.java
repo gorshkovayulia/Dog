afaf2aefbb57de38b34cd34a43deb2c9c35a0c3f
iuliia <gorshkovayulia93@gmail.com>
2022-05-01 16:23:21 +0300

    Add transaction management

--- src/main/java/com/example/dog/dao/JdbcDogDAO.java
+++ src/main/java/com/example/dog/dao/JdbcDogDAO.java
@@ -8,8 +8,9 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class JdbcDogDAO implements DogDAO {
+import static org.springframework.jdbc.support.JdbcUtils.closeConnection;
 
+public class JdbcDogDAO implements DogDAO {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("uuuu-MM-dd HH:mm:ss VV");
     private static final String SQL_SELECT_DOG =
             "select id, name, height, weight, birthday from dog where id = ?";
@@ -19,7 +20,6 @@ public class JdbcDogDAO implements DogDAO {
             "update dog set name = ?, height = ?, weight = ?, birthday = ? where id = ?";
     private static final String SQL_DELETE_DOG =
             "delete from dog where id = ?";
-
     private final DriverManagerDataSource dataSource;
 
     public JdbcDogDAO(DriverManagerDataSource dataSource) {
@@ -48,30 +48,39 @@ public class JdbcDogDAO implements DogDAO {
 
     @Override
     public Dog add(Dog dog) {
+        Connection conn = null;
         PreparedStatement statement;
-        try (Connection conn = dataSource.getConnection()) {
+        try {
+            conn = dataSource.getConnection();
+            conn.setAutoCommit(false);
             statement = conn.prepareStatement(SQL_INSERT_DOG, Statement.RETURN_GENERATED_KEYS);
             statement.setString(1, dog.getName());
             statement.setInt(2, dog.getHeight());
             statement.setInt(3, dog.getWeight());
             statement.setString(4, getDateString(dog.getDateOfBirth()));
             statement.executeUpdate();
+            conn.commit();
             try (ResultSet keys = statement.getGeneratedKeys()) {
                 if (keys.next()) {
                     dog.setId(keys.getInt(1));
                 }
             }
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            handleSQLException(conn, e);
+        } finally {
+            closeConnection(conn);
         }
         return dog;
     }
 
     @Override
     public Dog update(int id, Dog dog) {
+        Connection conn = null;
         PreparedStatement statement;
-        Dog updatedDog;
-        try (Connection conn = dataSource.getConnection()) {
+        Dog updatedDog = null;
+        try {
+            conn = dataSource.getConnection();
+            conn.setAutoCommit(false);
             statement = conn.prepareStatement(SQL_UPDATE_DOG);
             statement.setString(1, dog.getName());
             statement.setInt(2, dog.getHeight());
@@ -79,29 +88,38 @@ public class JdbcDogDAO implements DogDAO {
             statement.setString(4, getDateString(dog.getDateOfBirth()));
             statement.setInt(5, id);
             int affectedRows = statement.executeUpdate();
+            conn.commit();
             if (affectedRows == 0) {
                 throw new ObjectNotFoundException("Dog with id=" + id + " was not found!");
             }
             updatedDog = get(id);
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            handleSQLException(conn, e);
+        } finally {
+            closeConnection(conn);
         }
         return updatedDog;
     }
 
     @Override
     public Dog remove(int id) {
+        Connection conn = null;
         PreparedStatement statement;
         Dog removedDog = get(id);
-        try (Connection conn = dataSource.getConnection()) {
+        try {
+            conn = dataSource.getConnection();
+            conn.setAutoCommit(false);
             statement = conn.prepareStatement(SQL_DELETE_DOG);
             statement.setInt(1, id);
             int affectedRows = statement.executeUpdate();
+            conn.commit();
             if (affectedRows == 0) {
                 return null;
             }
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            handleSQLException(conn, e);
+        } finally {
+            closeConnection(conn);
         }
         return removedDog;
     }
@@ -112,5 +130,16 @@ public class JdbcDogDAO implements DogDAO {
 
     private String getDateString(ZonedDateTime time) {
         return time == null ? null : time.format(DATE_TIME_FORMATTER);
+    }
+    
+    private void handleSQLException(Connection conn, SQLException e) {
+        if (conn != null) {
+            try {
+                conn.rollback();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+        throw new RuntimeException(e);
     }
 }
