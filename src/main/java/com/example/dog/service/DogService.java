8b5e4a62a7b93d8a2570bcfdb89d4196736da96f
iuliia <gorshkovayulia93@gmail.com>
2022-05-22 18:39:48 +0300

    Remove annotation from DogService

--- src/main/java/com/example/dog/service/DogService.java
+++ src/main/java/com/example/dog/service/DogService.java
@@ -4,8 +4,6 @@ import com.example.dog.model.Dog;
 import com.example.dog.utils.CustomTransactional;
 
 public interface DogService {
-
-    @CustomTransactional
     Dog get(int id);
 
     Dog add(Dog dog);
