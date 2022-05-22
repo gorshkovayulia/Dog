e8eca4f5e349c614c385f46c66ca3b69238a7aa6
iuliia <gorshkovayulia93@gmail.com>
2022-05-22 18:45:27 +0300

    Remove CustomTransaction description

--- src/main/java/com/example/dog/utils/CustomTransactional.java
+++ src/main/java/com/example/dog/utils/CustomTransactional.java
@@ -5,9 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * This annotation is used for get method that requires only read-only mode
- */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD})
 public @interface CustomTransactional {
